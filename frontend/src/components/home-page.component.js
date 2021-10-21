dcc262eec5796de320354bf6cad6060e640ab026
sebastian920 <sstockbridge@ufl.edu>
2021-10-21 18:54:02 -0400

    Changed color of navbar and added description

--- frontend/src/components/home-page.component.js
+++ frontend/src/components/home-page.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './Stylesheet.css'
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -13,7 +14,10 @@ export default class HomePage extends Component {
     render() {
         return (
             <div>
-                <h3>Home Page</h3>
+                <h1 className="primary">Welcome</h1>
+                <h2 className="secondary">
+                GreenGator was created to help guide people towards becoming more environmentally conscious in every aspect of their lives. By tracking various elements of users' daily lives, GreenGator can measure users' carbon footprint to allow them to visualize the environmental impact of their daily routine, and show them how to improve.
+                </h2>
             </div>
         )
     }
--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">GreenGator</Link>
+            <nav className="navbar navbar-expand-lg" style={{backgroundColor:"#53be78", color:"black"}}>
+                <Link to="/" className="navbar-brand" style={{color:"black"}}>GreenGator</Link>
                 <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-item">
-                    <Link to="/login" className="nav-link">Login</Link> 
+                    <Link to="/login" className="nav-link" style={{color:"black"}}>Login</Link> 
                     </li>
                     <li className="navbar-item">
-                    <Link to="/create-account" className="nav-link">Create Account</Link> 
+                    <Link to="/create-account" className="nav-link" style={{color:"black"}}>Create Account</Link> 
                     </li>
                 </ul>
                 </div>
