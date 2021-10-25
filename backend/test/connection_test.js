f5398ebe3d7a9d61c31fad01ec5774aa972c43d7
sstockbridge <sstockbridge@ufl.edu>
2021-10-25 12:32:20 -0400

    Added more DB tests, including connection test

---
+++ backend/test/connection_test.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+
+mongoose.Promise = global.Promise;
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+const uri = process.env.ATLAS_URI;
+
+{/*Runs before the tests run*/}
+before(function(done) {
+    mongoose.connect(uri, { useNewUrlParser: true });
+    mongoose.connection.once("open", function() {
+        console.log("Connection has been made!");
+        done();
+        {/*mongoose.connection.close(true);*/}
+    }).on("error", function(error) {
+       console.log("Connection error", error); 
+    });
+});
--- backend/test/database_test.js
+++ backend/test/database_test.js
@@ -1,6 +1,7 @@
 const mocha = require("mocha");
 const assert = require("assert");
 const User = require("../models/user.model");
+const mongoose = require("mongoose");
 
 describe('Saving record', function() {
     it('Save a user in the database', function() {
@@ -17,4 +18,36 @@ describe('Saving record', function() {
             done(); {/*telling mocha that our test is done, can move on to next test*/}
         });
     });
+
+    it('Username length less than minLength', function() {
+        var user = new User({
+            username: 'U1',
+            password: '12345psW',
+            firstname: "Test",
+            lastname: "Test",
+            email: "test@gmail.com",
+            birth_date: new Date("December 17, 1995") 
+        });
+        user.save().then(function() {
+            assert(user.isNew === true); {/*true means that the user was not able to be saved to database*/}
+            done(); {/*telling mocha that our test is done, can move on to next test*/}
+        });
+    });
+
+    it('Password length less than minLength', function() {
+        var user = new User({
+            username: 'Username123',
+            password: '12345ps', 
+            firstname: "Test",
+            lastname: "Test",
+            email: "test@gmail.com",
+            birth_date: new Date("December 17, 1995") 
+        });
+        user.save().then(function() {
+            assert(user.isNew === true); {/*false when value is saved in the database after creation*/}
+            done(); {/*telling mocha that our test is done, can move on to next test*/}
+        });
+    });
+    
+    
 });
