dd9ecfe1b0d50ad6005729f11ee9933ff9f87b12
sstockbridge <sstockbridge@ufl.edu>
2021-10-25 16:41:36 -0400

    testing .yml file

--- backend/test/connection_test.js
+++ backend/test/connection_test.js
@@ -19,7 +19,7 @@ before(function(done) {
     mongoose.connection.once("open", function() {
         console.log("Connection has been made!");
         done();
-        {/*mongoose.connection.close(true);*/}
+        mongoose.connection.close(true);
     }).on("error", function(error) {
        console.log("Connection error", error); 
     });
